f747c8ed322286a49166ea66c97f20aa5a958926
jrfleming <jrfleming@noctrl.edu>
2025-05-05 02:34:22 -0500

    Changes based on old commit 9f6eeed

--- app/helpers/research_helper.rb
+++ app/helpers/research_helper.rb
@@ -1,25 +1,25 @@
 # app/helpers/research_helper.rb
 module ResearchHelper
-  # Returns a PubMed search URL for a given supplement and health goal
+  # Build a PubMed search URL from supplement + goal(s)
   def pubmed_search_url(supplement_name, goal)
-    # Convert to array if it's a JSON string (defensive fix)
-    goal_array =
+    goals =
       case goal
-      when String
-        goal.start_with?("[") ? JSON.parse(goal) : [goal]
       when Array
         goal
+      when String
+        str = goal.strip                       # ← remove leading/trailing spaces
+        if str.start_with?("[") && str.end_with?("]")
+          JSON.parse(str) rescue [str]         # try JSON-decode, fall back
+        else
+          [str]
+        end
       else
         [goal.to_s]
       end
-  
-    clean_goal = goal_array.reject(&:blank?).join(" ")
-    query = "#{supplement_name} #{clean_goal}".strip
-  
+
+    clean_goal = goals.reject(&:blank?).join(" ")
+    query       = "#{supplement_name} #{clean_goal}".strip
+
     "https://pubmed.ncbi.nlm.nih.gov/?term=#{CGI.escape(query)}"
-  rescue JSON::ParserError
-    "https://pubmed.ncbi.nlm.nih.gov/?term=#{CGI.escape(supplement_name)}"
   end
-  
-  
 end
